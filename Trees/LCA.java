6a81e73ae6c170b6fa551f4099bbe58ee33aa82d
Bhoomi Shetty <86159573+bhoomihz@users.noreply.github.com>
2023-08-23 13:28:10 +0530

    Create LCA.java

---
+++ Trees/LCA.java
@@ -0,0 +1,28 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; }
+ * }
+ */
+class Solution {
+    public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
+        //base case
+        if(root == null || root == p || root == q){
+            return root;
+        }
+        //recursion
+        TreeNode left = lowestCommonAncestor(root.left, p, q);
+        TreeNode right = lowestCommonAncestor(root.right, p, q);
+        //if any one side is null return the other side
+        if(left == null){
+            return right;
+        }else if(right == null){
+            return left;
+        }
+        return root; //if both are not null that means the root is our answer i.e. lowest ancestor.
+
+    }
+}
