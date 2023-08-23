31f420c3b7709f1693269c30067944cf6299fdbb
Bhoomi Shetty <86159573+bhoomihz@users.noreply.github.com>
2023-08-23 13:30:25 +0530

    Update Preorder.java

--- Trees/Preorder.java
+++ Trees/Preorder.java
@@ -1,1 +1,14 @@
+class Solution {
+    public List<Integer> preorderTraversal(TreeNode root) {
+        ArrayList<Integer> res = new ArrayList<>();
+        preOrder(root, res);
+        return res;
+    }
 
+    public void preOrder(TreeNode root, ArrayList<Integer> list){
+        if(root == null) return;
+        list.add(root.val);
+        preOrder(root.left, list);
+        preOrder(root.right, list);
+    }
+}
